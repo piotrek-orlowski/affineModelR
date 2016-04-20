299259e62ad3414753c258127a9ed66ccd1cd958
Piotrek Orłowski <piotrek.orlowski@gmail.com>
2016-04-20 14:23:11 +0000

    corrected simulation error in double exponential jumps, added function for choosing closest positive definite matrix for a square matrix that is not necessarily pos def

--- src/jumpGenerator.cpp
+++ src/jumpGenerator.cpp
@@ -14,7 +14,7 @@ arma::vec generate_expNormJump(arma::vec jmpPar){
   arma::vec stockJumpVol(1);
   stockJumpVol(0) = jmpPar(1);
   arma::vec volJumpMean(1);
-  volJumpMean(0) = jmpPar(2);
+  volJumpMean(0) = 1.0/jmpPar(2);
   arma::vec volJumpCorr(1);
   volJumpCorr(0) = jmpPar(3);
   
@@ -60,7 +60,7 @@ arma::vec generate_kouExpJump(arma::vec jmpPar){
   coinFlip(0) = 2 * coinFlip(0) - 1;
   
   // Generate stock jump
-  tmpRnd = rexp(1,volJumpMean(0));
+  tmpRnd = rexp(1,1.0/stockJumpVol(0));
   // tmpRnd = arma::randg(1, arma::distr_param(1.0,stockJumpVol(0)));
   tmpRnd(0) *= coinFlip(0);
   tmpRnd(0) += stockJumpMean(0);
